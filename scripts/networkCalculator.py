a3e6bcd3c626d06d63669380a1d2aed9f42d0312
joseph <j.b.siddall@gmail.com>
2013-04-22 21:40:57 +0100

    chapter 3:   - fixed some ref/label problems   - converted more tables to proper format   - updated python script to output latex table content

--- scripts/networkCalculator.py
+++ scripts/networkCalculator.py
@@ -7,6 +7,6 @@ def f(s, n): return s * n * 60.4
 
 for s in S_VALUES:
     for n in N_VALUES:
-	print "(s: %s, n: %s) = %1.2f kB" % (s, n, f(s, n)/1024.0)
+	print "    %s & %s & %1.2f \\\\" % (s, n, f(s, n)/1024.0)
 
 
